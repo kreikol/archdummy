ad8f51e560527388bf8041f9028236f04b0f8c6b
Arch Dummy <kreikol@gmail.com>
2023-06-25 12:01:16 +0000

    Init Arch Dummy dotfiles

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+
+# directorios NeoVim
+mkdir -p "$HOME/.config/nvim"
+mkdir -p "$HOME/.config/nvim/undo"
+
+ln -sf "$HOME/dotfiles/nvim/init.vim" "$HOME/.config/nvim/init.vim"
+
+# X11
+rm -rf "$HOME/.config/X11"
+ln -sf "$HOME/dotfiles/X11" "$HOME/.config/X11"
