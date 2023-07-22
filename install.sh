f909570e06197a06935b4f6070660535ff698728
Arch Dummy <kreikol@gmail.com>
2023-07-22 18:08:05 +0000

    feat (nvim): improve config and install plugin manager

--- install.sh
+++ install.sh
@@ -1,26 +1,46 @@
 #!/bin/bash
 #
 
+##
+# NeoVim
+##-----------------------------------------------
 # directorios NeoVim
 mkdir -p "$XDG_CONFIG_HOME/nvim"
 mkdir -p "$XDG_CONFIG_HOME/nvim/undo"
 
 ln -sf "$DOTFILES/nvim/init.vim" "$XDG_CONFIG_HOME/nvim/init.vim"
 
+# install neovim plugin manager
+[ ! -f "$DOTFILES/nvim/autoload/plug.vim" ] \
+    && curl -fLo "$DOTFILES/nvim/autoload/plug.vim" --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+mkdir -p "$XDG_CONFIG_HOME/nvim/autoload"
+ln -sf "$DOTFILES/nvim/autoload/plug.vim" "$XDG_CONFIG_HOME/nvim/autoload/plug.vim"
+
+# Install (or update) all the plugins
+nvim --noplugin +PlugUpdate +qa
+
+#
 # X11
+##-----------------------------------------------
 #rm -rf "$XDG_CONFIG_HOME/X11"
 #ln -s "$DOTFILES/X11" "$XDG_CONFIG_HOME/X11"
 rm -rf "$XDG_CONFIG_HOME/X11"
 ln -s "$DOTFILES/X11" "$XDG_CONFIG_HOME"
 
 # i3
+##-----------------------------------------------
 rm -rf "$XDG_CONFIG_HOME/i3"
 ln -s "$DOTFILES/i3" "$XDG_CONFIG_HOME/i3"
 
 # resolucion de pantalla
+##-----------------------------------------------
 ln -sf "$DOTFILES/screenlayout" "$HOME/.screenlayout"
 
+#
 # ZSH
+##-----------------------------------------------
 
 mkdir -p "$XDG_CONFIG_HOME/zsh"
 ln -sf "$DOTFILES/zsh/.zshenv" "$HOME"
@@ -29,20 +49,26 @@ ln -sf "$DOTFILES/zsh/aliases" "$XDG_CONFIG_HOME/zsh/aliases"
 rm -rf "$XDG_CONFIG_HOME/zsh/external"
 ln -sf "$DOTFILES/zsh/external" "$XDG_CONFIG_HOME/zsh"
 
+#
 # Fonts
+##-----------------------------------------------
 
 mkdir -p "$XDG_DATA_HOME"
 cp -rf "$DOTFILES/fonts" "$XDG_DATA_HOME"
 
+#
 # Notifications: dunst
+##-----------------------------------------------
 
 mkdir -p "$XDG_DATA_HOME/dunst"
 cp -rf "$DOTFILES/dunst/dunstrc" "$XDG_DATA_HOME/dunst/dunstrc"
 
 # UI style GTK3
+##-----------------------------------------------
 rm -rf "$XDG_CONFIG_HOME/gtk-3.0"
 ln -s "$DOTFILES/gtk-3.0" "$XDG_CONFIG_HOME/gtk-3.0"
 
 # Git
+##-----------------------------------------------
 rm -rf "$XDG_CONFIG_HOME/git"
 ln -s "$DOTFILES/git" "$XDG_CONFIG_HOME/git"
--- nvim/init.vim
+++ nvim/init.vim
@@ -1,9 +1,24 @@
+" Plugins - con vim-plug
+call plug#begin("$XDG_CONFIG_HOME/nvim/plugged")
+    Plug 'chrisbra/csv.vim'
+    Plug 'moll/vim-bbye' " cerrar buffer sin cerrar ventana
+    Plug 'simeji/winresizer' " easy resize
+    Plug 'junegunn/fzf.vim'
+    Plug 'simnalamburt/vim-mundo'
+call plug#end()
+
 set clipboard+=unnamedplus
 
 noremap <Up> <Nop>
 noremap <Down> <Nop>
 noremap <Left> <Nop>
 noremap <Right> <Nop>
+
+" Jumps 
+"   g; - cambio anterior
+"   g, - cambio siguiente
+"   [m - hace el inicio de un método ([)
+"   ]m - hace el final de un método (])
 
 set noswapfile
 
@@ -27,5 +42,29 @@ set tabstop=4
 set softtabstop=4
 set shiftwidth=4
 
-" Show substitution
+" Show substitutios
 set inccommand=nosplit
+
+nnoremap <space> <nop>
+let mapleader = "\<space>"
+
+nnoremap <leader>bn :bn<cr> ;buffer next
+nnoremap <leader>bp :bp<cr> ;buffer prev
+nnoremap <leader>bd :Bdelete<cr> ;cerrar buffer sin cerrar la ventana
+nnoremap <leader>tn gt ;new tab
+
+" Guardar y salir
+nnoremap <leader>w :w<cr>
+nnoremap <leader>q :q<cr>
+nnoremap <leader>q! :q!<cr>
+
+" Config for chrisbra/csv.vim
+augroup filetype_csv
+    autocmd! 
+
+    autocmd BufRead,BufWritePost *.csv :%ArrangeColumn!
+    autocmd BufWritePre *.csv :%UnArrangeColumn
+augroup END
+
+" Config for fzf.vim (BONUS :D)
+nnoremap <leader>f :Files<cr>
